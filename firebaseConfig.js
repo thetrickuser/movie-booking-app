b387ddb512d6c9997f1e14cf3ea216e0d6f3c556
thetrickuser <coolady1807@outlook.com>
2024-09-21 20:15:30 +0530

    removed hardcoded firebase config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,12 +3,7 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC2PxjS7IF5PxCqWb4uWPJRXnnTwW6LfgI",
-  authDomain: "moviesmagic-105ec.firebaseapp.com",
-  projectId: "moviesmagic-105ec",
-  storageBucket: "moviesmagic-105ec.appspot.com",
-  messagingSenderId: "75949901427",
-  appId: "1:75949901427:web:6b6bcf4e9e4eb3b2478c06"
+  
 };
 
 const app = initializeApp(firebaseConfig);
